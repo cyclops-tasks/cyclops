4c25dcdd757e48e1205636dd9101776ae4b0ba01
winton <mail@wintoni.us>
2018-08-25 03:10:41 -0400

    Test entire task run state

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,16 +1,48 @@
-import { run } from "../dist/cyclops"
+import { createStore, run } from "../dist/cyclops"
 
-test("run task", async () => {
-  const store = await run({
+async function runTask() {
+  const store = createStore()
+  await store.set("cyclops.options", {
     _: ["task"],
     path: `${__dirname}/fixture`,
   })
+  await run(store)
+  return store
+}
 
+test("run task", async () => {
+  const store = await runTask()
   const tasks = store.get("cyclops.tasks")
+  const taskIds = Object.keys(tasks)
 
-  expect(Object.keys(tasks).length).toBe(1)
-
-  for (const id in tasks) {
-    expect(tasks[id].ran).toBe(true)
-  }
+  expect(store.state).toEqual({
+    cyclops: {
+      options: {
+        _: ["task"],
+        path: `${__dirname}/fixture`,
+      },
+      tasks: {
+        [taskIds[0]]: {
+          paths: {
+            bin: `${__dirname}/fixture/project-a/node_modules/module-a/lib/a.js`,
+            link: `${__dirname}/fixture/project-a/node_modules/.bin/task`,
+            pkg: `${__dirname}/fixture/project-a/node_modules/module-a/package.json`,
+            project: `${__dirname}/fixture/project-a`,
+            task: `${__dirname}/fixture/project-a/node_modules/module-a`,
+          },
+          ran: true,
+        },
+      },
+    },
+    glob: {
+      cyclops: {
+        ignore: ["**/node_modules/**"],
+        paths: [
+          `${__dirname}/fixture/project-a/node_modules/module-a/package.json`,
+          `${__dirname}/fixture/project-a/package.json`,
+        ],
+        pattern: `${__dirname}/fixture/**/package.json`,
+      },
+    },
+  })
 })
