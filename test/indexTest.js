82e7d8ad52f5b61854c621d5eee71ca3c76eee04
winton <mail@wintoni.us>
2018-08-25 02:18:29 -0400

    Simple test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,16 @@
-import * as lib from "../dist"
+import { run } from "../dist/cyclops"
 
-test("export something", () => {
-  expect(Object.keys(lib).length).toBeGreaterThan(0)
+test("run task", async () => {
+  const store = await run({
+    _: ["task"],
+    path: `${__dirname}/fixture`,
+  })
+
+  const tasks = store.get("cyclops.tasks")
+
+  expect(Object.keys(tasks).length).toBe(1)
+
+  for (const id in tasks) {
+    expect(tasks[id].ran).toBe(true)
+  }
 })
