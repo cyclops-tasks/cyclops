6dba94cfa6bfd2dd6d05ba6756db7f191e1d0efb
winton <mail@wintoni.us>
2018-09-24 14:30:42 -0400

    Upgrading packages

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,46 +1,34 @@
-import { createStore, run } from "../dist/cyclops"
+import { composeStore } from "../dist/cyclops"
 
 async function runTask() {
-  const store = createStore()
-  await run({
-    argv: false,
+  const store = composeStore()
+  await store.cyclops({
+    argv: ["task"],
     path: `${__dirname}/fixture`,
-    store,
-    task: "task",
   })
   return store
 }
 
 test("run task", async () => {
   const store = await runTask()
-  const tasks = store.get("cyclops.tasks")
+  const tasks = store.get("tasks")
   const taskIds = Object.keys(tasks)
 
   expect(store.state).toEqual({
-    cyclops: {
-      options: {
-        _: ["task"],
-        path: `${__dirname}/fixture`,
-      },
-      tasks: {
-        [taskIds[0]]: {
-          paths: {
-            bin: `${__dirname}/fixture/project-a/node_modules/module-a/lib/a.js`,
-            pkg: `${__dirname}/fixture/project-a/node_modules/module-a/package.json`,
-            project: `${__dirname}/fixture/project-a`,
-            task: `${__dirname}/fixture/project-a/node_modules/module-a`,
-          },
-          ran: true, // set from test task
-        },
-      },
+    argv: {
+      _: ["task"],
     },
-    glob: {
-      cyclops: {
-        ignore: ["**/node_modules/**"],
-        paths: [
-          `${__dirname}/fixture/project-a/package.json`,
-        ],
-        pattern: `${__dirname}/fixture/**/package.json`,
+    packageJsonPaths: [
+      `${__dirname}/fixture/project-a/package.json`,
+    ],
+    path: `${__dirname}/fixture`,
+    tasks: {
+      [taskIds[0]]: {
+        binPath: `${__dirname}/fixture/project-a/node_modules/module-a/lib/a.js`,
+        pkgPath: `${__dirname}/fixture/project-a/node_modules/module-a/package.json`,
+        projectPath: `${__dirname}/fixture/project-a`,
+        ran: true,
+        taskPath: `${__dirname}/fixture/project-a/node_modules/module-a`,
       },
     },
   })
